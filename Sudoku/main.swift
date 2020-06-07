6a215255c0e321bc0211a1bb7a08eef1c66a97a0
Zhengyu Ren <renzhengyu@gmail.com>
2020-06-07 19:19:00 +0700

    Initial Commit

---
+++ Sudoku/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sudoku
+//
+//  Created by Zhengyu Ren on 7/6/20.
+//  Copyright © 2020 Zhengyu Ren. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
